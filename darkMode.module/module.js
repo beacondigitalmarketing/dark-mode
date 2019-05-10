5d98f14b90caf1d79366b2cc4c6dfe3ba2e288ae
dennis <dennis@beacondigitalmarketing.com>
2019-05-10 09:07:24 -0400

    ran js through babel

--- darkMode.module/module.js
+++ darkMode.module/module.js
@@ -1,8 +1,10 @@
-const toggleSwitch = document.querySelector(
+"use strict";
+
+var toggleSwitch = document.querySelector(
   '.theme-toggle input[type="checkbox"]'
 );
 
-function switchTheme(e) { 
+function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
@@ -12,24 +14,22 @@ function switchTheme(e) {
   }
 }
 
-const currentTheme = localStorage.getItem("theme")
+var currentTheme = localStorage.getItem("theme")
   ? localStorage.getItem("theme")
   : null;
-if (typeof currentTheme == "undefined"){
+
+if (typeof currentTheme == "undefined") {
   console.log("theme not stored");
-  if(matchMedia('(prefers-color-scheme: dark)').matches) {
+
+  if (matchMedia("(prefers-color-scheme: dark)").matches) {
     console.log("prefer dark");
-          document.documentElement.setAttribute("data-theme", "dark");
-  
-          toggleSwitch.checked = true;
-    
-  }
-  else if(matchMedia('(prefers-color-scheme: light)').matches){
+    document.documentElement.setAttribute("data-theme", "dark");
+    toggleSwitch.checked = true;
+  } else if (matchMedia("(prefers-color-scheme: light)").matches) {
     console.log("prefers light");
-          document.documentElement.setAttribute("data-theme", "light");
-          toggleSwitch.checked = false;
+    document.documentElement.setAttribute("data-theme", "light");
+    toggleSwitch.checked = false;
   }
-  
 }
 
 if (currentTheme) {
@@ -39,17 +39,19 @@ if (currentTheme) {
     toggleSwitch.checked = true;
   }
 }
-toggleSwitch.addEventListener("change", switchTheme, false);
 
-//get header height and add some distance to main
-const header = document.querySelector("header");
-const main = document.querySelector("header");
+toggleSwitch.addEventListener("change", switchTheme, false); //get header height and add some distance to main
 
+var header = document.querySelector("header");
+var main = document.querySelector("header");
 
 function space() {
-  let headerHeight = header.clientHeight + "px";
-  document.querySelector("main").style.marginTop = headerHeight
-  console.log(headerHeight);  
+  var headerHeight = header.clientHeight + "px";
+  document.querySelector("main").style.marginTop = headerHeight;
+  console.log(headerHeight);
 }
+
 window.addEventListener("load", space);
 window.addEventListener("resize", space);
+
+//# sourceMappingURL=scripts-min.js.map
